2518266c919f676adbbf573c194ccc66db240dce
Jonas Plum <git@cugu.eu>
2020-06-02 14:30:48 +0200

    Add OSFS support

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='forensicstore',
-    version='0.16.1',
+    version='0.16.2',
     url='https://github.com/forensicanalysis/pyforensicstore',
     author='Jonas Plum',
     author_email='jonas.plum@siemens.com',
